f24c085dd119a547ed2e98559c7074f6588baaaa
Philipp-Lorenz Glaser <39776671+plglaser@users.noreply.github.com>
2018-11-22 16:17:30 +0100

    Add files via upload

---
+++ Tiss ECTS Display.user.js
@@ -0,0 +1,47 @@
+// ==UserScript==
+// @name         Tiss ECTS Display
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  basic script to display ECTS at the top right corner
+// @author       Philipp-Lorenz Glaser
+// @match        https://tiss.tuwien.ac.at/education/favorites.*
+// @grant        none
+// @require http://code.jquery.com/jquery-latest.js
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+
+    var $ = window.jQuery;
+
+    var $certs1 = $("tr.ui-widget-content.ui-datatable-even > td.favoritesCert").has("span");
+    var $certs2 = $("tr.ui-widget-content.ui-datatable-odd > td.favoritesCert").has("span");
+
+    var $rows = [];
+    
+    for(var i = 0; i < $certs1.length; i++){
+        ($rows).push($certs1.get(i).parentNode);
+    }
+
+    for(var j = 0; j < $certs2.length; j++){
+        ($rows).push($certs2.get(j).parentNode);
+    }
+
+    var ECTS = 0;
+
+    for(var k = 0; k < $rows.length; k++){
+        var pointA = $($rows[k]).find("td.favoritesECTS").text();
+        ECTS += Number(pointA);
+    }
+
+    if (ECTS>0){
+        $("h1").append('<div class="ects"><div>');
+        $("div.ects").css("display", "inline-block");
+        $("div.ects").css("float", "right");
+        $("div.ects").text("ECTS: "+ ECTS);
+
+    }
+
+
+})();
