0aaab93fccf41da09113964958339637180a3e7f
Philipp-Lorenz Glaser <39776671+plglaser@users.noreply.github.com>
2019-03-08 02:58:46 +0100

    New features

--- Tiss ECTS Display.user.js
+++ Tiss ECTS Display.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Tiss ECTS Display
 // @namespace    http://tampermonkey.net/
-// @version      0.2
+// @version      0.3
 // @description  basic script to display ECTS at the top right corner
 // @author       Philipp-Lorenz Glaser
 // @match        https://tiss.tuwien.ac.at/education/favorites.*
@@ -10,18 +10,23 @@
 // ==/UserScript==
 
 (function() {
+    if ( window.history.replaceState ) {
+        window.history.replaceState( null, null, window.location.href );
+    }
+
     'use strict';
     var $ = window.jQuery;
 
     var $certs1 = $("tr.ui-widget-content.ui-datatable-even > td.favoritesCert").has("span");
+    $("tr.ui-widget-content.ui-datatable-even").find(".favoritesActionCol2").append(' <input type="checkbox" class="box">')
     var $certs2 = $("tr.ui-widget-content.ui-datatable-odd > td.favoritesCert").has("span");
+    $("tr.ui-widget-content.ui-datatable-odd").find(".favoritesActionCol2").append( ' <input type="checkbox" class="box">')
 
     var $rows = [];
-    
+
     for(var i = 0; i < $certs1.length; i++){
         ($rows).push($certs1.get(i).parentNode);
     }
-
     for(var j = 0; j < $certs2.length; j++){
         ($rows).push($certs2.get(j).parentNode);
     }
@@ -47,5 +52,22 @@
         $("div.ects").css("float", "right");
         $("div.ects").text("ECTS: "+ ECTS + " / " + addedECTS);
     }
+
+    var temp = 0;
+    $(document).ready(function() {
+        $('.box').on('change', function(event) {
+
+               var checked = event.target;
+               temp = $(checked.parentNode.parentNode).find("td.favoritesECTS").text();
+               if($(checked).prop("checked") == true){
+                   ECTS += Number(temp);
+               }
+               else {
+                   ECTS -= Number(temp);
+               }
+               $("div.ects").text("ECTS: "+ ECTS + " / " + addedECTS);
+
+        });
+    });
 
 })();
